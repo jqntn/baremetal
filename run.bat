7a8a73205b108f5b8c961b0747010cfaa101886e
Julien QUENTIN <jqntn88@gmail.com>
2025-10-05 15:06:58 +0200

    ld.lld

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-clang++ --target=i386-elf -ffreestanding -fno-exceptions -fno-rtti -c kernel.cpp -o kernel.o
-i386-elf-ld -Ttext 0x100000 kernel.o -o kernel.elf
+clang++ -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c kernel.cpp -o kernel.o
+ld.lld kernel.o -o kernel.elf
 qemu-system-i386 -kernel kernel.elf -display sdl
