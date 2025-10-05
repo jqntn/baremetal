2c965cd8a2ebc5fffa2199db60a57891d0519088
Julien QUENTIN <jqntn88@gmail.com>
2025-10-05 13:33:19 +0200

    format

--- kernel.cpp
+++ kernel.cpp
@@ -1,25 +1,24 @@
-extern "C" {
+extern "C"
+{
+  asm(".section .multiboot;"
+      ".align 4;"
+      "multiboot_header:;"
+      ".long 0x1BADB002;"
+      ".long 0x0;"
+      ".long -(0x1BADB002+0);");
 
-asm(
-    ".section .multiboot;"
-    ".align 4;"
-    "multiboot_header:;"
-    ".long 0x1BADB002;"
-    ".long 0x0;"
-    ".long -(0x1BADB002+0);"
-);
-
-void _start() {
+  void _start()
+  {
     volatile unsigned short* vga = (unsigned short*)0xB8000;
     const char* msg = "Hello, world!";
     int pos = 0;
 
     while (*msg) {
-        vga[pos++] = ((*msg) & 0xFF) | (0x0F << 8);
-        ++msg;
+      vga[pos++] = ((*msg) & 0xFF) | (0x0F << 8);
+      ++msg;
     }
 
-    for(;;);
-}
-
+    for (;;)
+      ;
+  }
 }
