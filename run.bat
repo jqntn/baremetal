c1e7766314b025495363c0723666ef37329d2495
Julien QUENTIN <jqntn88@gmail.com>
2025-10-05 14:13:39 +0200

    clang

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-i386-elf-g++ -ffreestanding -fno-exceptions -fno-rtti -c kernel.cpp -o kernel.o
+clang++ --target=i386-elf -ffreestanding -fno-exceptions -fno-rtti -c kernel.cpp -o kernel.o
 i386-elf-ld -Ttext 0x100000 kernel.o -o kernel.elf
 qemu-system-i386 -kernel kernel.elf -display sdl
