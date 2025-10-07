a89839b55b4d77fdc30e3915922ccda3422057e1
Julien QUENTIN <jqntn88@gmail.com>
2025-10-08 00:26:18 +0200

    verbose run.bat

--- run.bat
+++ run.bat
@@ -1,11 +1,13 @@
+set CLANG_FLAGS=-c --target=i386-elf -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -fno-strict-aliasing
+
 md bin
 md boot
 
-clang -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c thirdparty\xvg\vga\vga.c -o bin\vga.o
-clang -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c thirdparty\xvg\sgl\sgl.c -o bin\sgl.o
+clang %CLANG_FLAGS% thirdparty\xvg\vga\vga.c -o bin\vga.o
+clang %CLANG_FLAGS% thirdparty\xvg\sgl\sgl.c -o bin\sgl.o
 
-clang++ -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c kernel.cpp -o bin\kernel.o
+clang++ %CLANG_FLAGS% kernel.cpp -o bin\kernel.o
 
-ld.lld -m elf_i386 bin\kernel.o bin\vga.o bin\sgl.o -o boot\kernel.elf
+ld.lld -m elf_i386 --Ttext=0x100000 --image-base=0x100000 --omagic --nostdlib --static bin\kernel.o bin\vga.o bin\sgl.o -o boot\kernel.elf
 
 qemu-system-i386 -kernel boot\kernel.elf -display sdl -vga std
