d8a02c42070040ceebdc6cd7ccd165a60fdee9dd
Julien QUENTIN <jqntn88@gmail.com>
2025-10-07 23:54:05 +0200

    update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,11 @@
-clang -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c thirdparty\xvg\vga\vga.c -o vga.o
-clang -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c thirdparty\xvg\sgl\sgl.c -o sgl.o
+md bin
+md boot
 
-clang++ -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c kernel.cpp -o kernel.o
+clang -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c thirdparty\xvg\vga\vga.c -o bin\vga.o
+clang -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c thirdparty\xvg\sgl\sgl.c -o bin\sgl.o
 
-ld.lld -m elf_i386 kernel.o vga.o sgl.o -o kernel.elf
+clang++ -target i386-elf -ffreestanding -fno-exceptions -fno-rtti -c kernel.cpp -o bin\kernel.o
 
-qemu-system-i386 -kernel kernel.elf -display sdl -vga std
+ld.lld -m elf_i386 bin\kernel.o bin\vga.o bin\sgl.o -o boot\kernel.elf
+
+qemu-system-i386 -kernel boot\kernel.elf -display sdl -vga std
