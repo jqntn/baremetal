732ff7c0810210582a0855b85188f2d86b7ad3b7
Julien QUENTIN <jqntn88@gmail.com>
2025-10-09 01:40:43 +0200

    bootloader

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,7 @@
 extern "C"
 {
+#include <stdint.h>
+
 #include "thirdparty/xvg/sgl/sgl.h"
 #include "thirdparty/xvg/vga/vga.h"
 
@@ -34,8 +36,125 @@ extern "C"
     return ret;
   }
 
+  /**
+   * Display (extremely minimal) formated message on serial
+   * %c: an ASCII character
+   * %d: a decimal number
+   * %x: a hexadecimal number
+   * %p: a pointer
+   * %s: a zero terminated ASCII string (8 bit)
+   * %S: a zero terminated WCHAR string (16 bit characters, truncated to 8 bit)
+   * %D: dump 16 bytes from given address
+   */
+  void printf(const char* fmt, ...)
+  {
+    __builtin_va_list args;
+    int arg, len, sign, i;
+    unsigned int uarg;
+    char *p, tmpstr[19], n;
+    /* macro to put a character on serial console */
+#ifdef __aarch64__
+#define mmio_base 0x3F000000
+#define UART0_DR ((volatile uint32_t*)(mmio_base + 0x00201000))
+#define UART0_FR ((volatile uint32_t*)(mmio_base + 0x00201018))
+#define PUTC(c)                                                                \
+  do {                                                                         \
+    do {                                                                       \
+      __asm__ __volatile__("nop");                                             \
+    } while (*UART0_FR & 0x20);                                                \
+    *UART0_DR = c;                                                             \
+  } while (0)
+#else
+#define PUTC(c)                                                                \
+  __asm__ __volatile__(                                                        \
+    "xorl %%ebx, %%ebx; movb %0, %%bl;"                                        \
+    "movl $10000,%%ecx;"                                                       \
+    "1:inb %%dx, %%al;pause;"                                                  \
+    "cmpb $0xff,%%al;je 2f;"                                                   \
+    "dec %%ecx;jz 2f;"                                                         \
+    "andb $0x20,%%al;jz 1b;"                                                   \
+    "subb $5,%%dl;movb %%bl, %%al;outb %%al, %%dx;2:" ::"a"(c),                \
+    "d"(0x3fd)                                                                 \
+    : "rbx", "rcx");
+#endif
+    /* parse format and print */
+    __builtin_va_start(args, fmt);
+    arg = 0;
+    while (*fmt) {
+      if (*fmt == '%') {
+        fmt++;
+        if (*fmt == '%')
+          goto put;
+        len = 0;
+        while (*fmt >= '0' && *fmt <= '9') {
+          len *= 10;
+          len += *fmt - '0';
+          fmt++;
+        }
+        if (*fmt == 'c') {
+          arg = __builtin_va_arg(args, int);
+          PUTC((uint8_t)arg);
+          fmt++;
+          continue;
+        } else if (*fmt == 'd') {
+          arg = __builtin_va_arg(args, int);
+          sign = 0;
+          if ((int)arg < 0) {
+            arg = -arg;
+            sign++;
+          }
+          i = 18;
+          tmpstr[i] = 0;
+          do {
+            tmpstr[--i] = '0' + (arg % 10);
+            arg /= 10;
+          } while (arg != 0 && i > 0);
+          if (sign)
+            tmpstr[--i] = '-';
+          if (len > 0 && len < 18) {
+            while (i > 18 - len)
+              tmpstr[--i] = ' ';
+          }
+          p = &tmpstr[i];
+          goto putstring;
+        } else if (*fmt == 'x') {
+          uarg = __builtin_va_arg(args, unsigned int);
+          i = 16;
+          tmpstr[i] = 0;
+          do {
+            n = uarg & 0xf;
+            tmpstr[--i] = n + (n > 9 ? 0x37 : 0x30);
+            uarg >>= 4;
+          } while (uarg != 0 && i > 0);
+          if (len > 0 && len <= 16) {
+            while (i > 16 - len)
+              tmpstr[--i] = '0';
+          }
+          p = &tmpstr[i];
+          goto putstring;
+        } else if (*fmt == 's') {
+          p = __builtin_va_arg(args, char*);
+        putstring:
+          if (p == (void*)0)
+            p = (char*)"(null)";
+          while (*p)
+            PUTC(*p++);
+        }
+      } else {
+      put:
+        PUTC(*fmt);
+      }
+      fmt++;
+    }
+    __builtin_va_end(args);
+  }
+
   void _start()
   {
+    printf("Hello, world!\n");
+
+    xvInitGfxMode(MODE03H);
+
     volatile unsigned short* vgat = (unsigned short*)0xB8000;
 
     for (int i = 0; i < 80 * 25; ++i)
@@ -44,8 +163,6 @@ extern "C"
     const char* msg = "Hello, world!";
     for (int i = 0; msg[i]; ++i)
       vgat[i] = (0x0F << 8) | msg[i];
-
-    // xvInitGfxMode(MODE13H);
 
     for (;;)
       ;
--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
+@echo off
+
 set CLANG_FLAGS=-c --target=i386-elf -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -fno-strict-aliasing
 
-md bin
-md boot
+if not exist bin md bin
+if not exist boot md boot
 
 clang %CLANG_FLAGS% thirdparty\xvg\vga\vga.c -o bin\vga.o
 clang %CLANG_FLAGS% thirdparty\xvg\sgl\sgl.c -o bin\sgl.o
@@ -10,4 +12,8 @@ clang++ %CLANG_FLAGS% kernel.cpp -o bin\kernel.o
 
 ld.lld -m elf_i386 --entry=_start --Ttext=0x100000 --image-base=0x100000 --omagic --nostdlib --static bin\kernel.o bin\vga.o bin\sgl.o -o boot\kernel.elf
 
-qemu-system-x86_64 -kernel boot\kernel.elf -display sdl -vga std
+::qemu-system-x86_64 -kernel boot\kernel.elf -display sdl -vga std -serial stdio
+
+simpleboot -vv boot disk.img
+
+qemu-system-x86_64 -drive file=disk.img,format=raw -display sdl -vga std -serial stdio
