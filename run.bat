d3ea36102e81082aa81a03a5841a2498982d062e
Julien QUENTIN <jqntn88@gmail.com>
2025-10-09 02:09:56 +0200

    x86_64 kernel

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CLANG_FLAGS=-c --target=i386-elf -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -fno-strict-aliasing
+set CLANG_FLAGS=-c --target=x86_64-elf -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -fno-strict-aliasing
 
 if not exist bin md bin
 if not exist boot md boot
@@ -10,7 +10,7 @@ clang %CLANG_FLAGS% thirdparty\xvg\sgl\sgl.c -o bin\sgl.o
 
 clang++ %CLANG_FLAGS% kernel.cpp -o bin\kernel.o
 
-ld.lld -m elf_i386 --entry=_start --Ttext=0x100000 --image-base=0x100000 --omagic --nostdlib --static bin\kernel.o bin\vga.o bin\sgl.o -o boot\kernel.elf
+ld.lld -m elf_x86_64 --entry=_start --Ttext=0x100000 --image-base=0x100000 --omagic --nostdlib --static bin\kernel.o bin\vga.o bin\sgl.o -o boot\kernel.elf
 
 simpleboot -vv boot disk.img
 
