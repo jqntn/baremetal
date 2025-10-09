d5e6860bb2822c8117b262d6f6cdf62f2f49062b
Julien QUENTIN <jqntn88@gmail.com>
2025-10-09 23:12:20 +0200

    more portable

--- kernel.cpp
+++ kernel.cpp
@@ -1,13 +1,20 @@
 extern "C"
 {
-#include <stdint.h>
+#include <stdarg.h>
 
 #include "thirdparty/xvg/sgl/sgl.h"
 #include "thirdparty/xvg/vga/vga.h"
 
+  asm(".section .multiboot;"
+      ".align 4;"
+      "multiboot_header:;"
+      ".long 0x1BADB002;"
+      ".long 0;"
+      ".long -(0x1BADB002+0);");
+
   void outb(unsigned short port, unsigned char val)
   {
-    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+    asm volatile("outb %0, %1" ::"a"(val), "Nd"(port));
   }
 
   unsigned char insb(unsigned short port)
@@ -19,7 +26,7 @@ extern "C"
 
   void outl(unsigned short port, unsigned int val)
   {
-    asm volatile("outl %0,%1" : : "a"(val), "Nd"(port));
+    asm volatile("outl %0,%1" ::"a"(val), "Nd"(port));
   }
 
   unsigned int insl(unsigned short port)
@@ -41,37 +48,23 @@ extern "C"
    */
   void printf(const char* fmt, ...)
   {
-    __builtin_va_list args;
+    va_list args;
     int arg, len, sign, i;
     unsigned int uarg;
     char *p, tmpstr[19], n;
     /* macro to put a character on serial console */
-#ifdef __aarch64__
-#define mmio_base 0x3F000000
-#define UART0_DR ((volatile uint32_t*)(mmio_base + 0x00201000))
-#define UART0_FR ((volatile uint32_t*)(mmio_base + 0x00201018))
 #define PUTC(c)                                                                \
-  do {                                                                         \
-    do {                                                                       \
-      __asm__ __volatile__("nop");                                             \
-    } while (*UART0_FR & 0x20);                                                \
-    *UART0_DR = c;                                                             \
-  } while (0)
-#else
-#define PUTC(c)                                                                \
-  __asm__ __volatile__(                                                        \
-    "xorl %%ebx, %%ebx; movb %0, %%bl;"                                        \
-    "movl $10000,%%ecx;"                                                       \
-    "1:inb %%dx, %%al;pause;"                                                  \
-    "cmpb $0xff,%%al;je 2f;"                                                   \
-    "dec %%ecx;jz 2f;"                                                         \
-    "andb $0x20,%%al;jz 1b;"                                                   \
-    "subb $5,%%dl;movb %%bl, %%al;outb %%al, %%dx;2:" ::"a"(c),                \
-    "d"(0x3fd)                                                                 \
-    : "rbx", "rcx");
-#endif
+  asm volatile("xorl %%ebx, %%ebx; movb %0, %%bl;"                             \
+               "movl $10000,%%ecx;"                                            \
+               "1:inb %%dx, %%al;pause;"                                       \
+               "cmpb $0xff,%%al;je 2f;"                                        \
+               "dec %%ecx;jz 2f;"                                              \
+               "andb $0x20,%%al;jz 1b;"                                        \
+               "subb $5,%%dl;movb %%bl, %%al;outb %%al, %%dx;2:" ::"a"(c),     \
+               "d"(0x3fd)                                                      \
+               : "rbx", "rcx");
     /* parse format and print */
-    __builtin_va_start(args, fmt);
+    va_start(args, fmt);
     arg = 0;
     while (*fmt) {
       if (*fmt == '%') {
@@ -85,12 +78,12 @@ extern "C"
           fmt++;
         }
         if (*fmt == 'c') {
-          arg = __builtin_va_arg(args, int);
-          PUTC((uint8_t)arg);
+          arg = va_arg(args, int);
+          PUTC((unsigned char)arg);
           fmt++;
           continue;
         } else if (*fmt == 'd') {
-          arg = __builtin_va_arg(args, int);
+          arg = va_arg(args, int);
           sign = 0;
           if ((int)arg < 0) {
             arg = -arg;
@@ -111,7 +104,7 @@ extern "C"
           p = &tmpstr[i];
           goto putstring;
         } else if (*fmt == 'x') {
-          uarg = __builtin_va_arg(args, unsigned int);
+          uarg = va_arg(args, unsigned int);
           i = 16;
           tmpstr[i] = 0;
           do {
@@ -126,7 +119,7 @@ extern "C"
           p = &tmpstr[i];
           goto putstring;
         } else if (*fmt == 's') {
-          p = __builtin_va_arg(args, char*);
+          p = va_arg(args, char*);
         putstring:
           if (p == (void*)0)
             p = (char*)"(null)";
@@ -139,7 +132,7 @@ extern "C"
       }
       fmt++;
     }
-    __builtin_va_end(args);
+    va_end(args);
   }
 
   void vgat_hello_world()
@@ -157,6 +150,10 @@ extern "C"
   void _start()
   {
     printf("Hello, world!\n");
+
+    vgat_hello_world();
+
+    xvInitGfxMode(MODE13H);
 
     for (;;)
       ;
---
+++ run-km.bat
@@ -0,0 +1,23 @@
+@echo off
+
+setlocal
+
+set CLANG_FLAGS=-c --target=i386-elf -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -fno-strict-aliasing -fno-builtin
+set LD_FLAGS=-m elf_i386 --entry=_start --Ttext=0x100000 --image-base=0x100000 --omagic --nostdlib --static
+
+if not exist bin md bin
+if not exist obj md obj
+if not exist boot md boot
+
+clang++ %CLANG_FLAGS% "kernel.cpp" -o "obj\kernel.o"
+clang %CLANG_FLAGS% "thirdparty\xvg\vga\vga.c" -o "obj\vga.o"
+clang %CLANG_FLAGS% "thirdparty\xvg\sgl\sgl.c" -o "obj\sgl.o"
+
+ld.lld %LD_FLAGS% "obj\kernel.o" "obj\vga.o" "obj\sgl.o" -o "boot\kernel.elf"
+
+if errorlevel 1 exit /b 1
+
+"thirdparty\simpleboot\simpleboot.exe" -vv -c -k "kernel.elf" "boot" "bin\disk.img"
+qemu-system-x86_64 -kernel "boot\kernel.elf" -display sdl -vga std -serial stdio
+
+endlocal
