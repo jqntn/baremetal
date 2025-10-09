acce0314ce066ca3792bbb8bbb37b651a548b87c
Julien QUENTIN <jqntn88@gmail.com>
2025-10-09 22:47:24 +0200

    wip msvc support

---
+++ run-msvc.bat
@@ -0,0 +1,23 @@
+@echo off
+
+setlocal
+
+set CL_FLAGS=/nologo /c /Zl /EHs-c- /GR- /GS- /Oi /Fo"obj\\"
+set LINK_FLAGS=/NOLOGO /MACHINE:X64 /ENTRY:_start /BASE:0x100000 /NODEFAULTLIB /SUBSYSTEM:NATIVE /DRIVER /MERGE:.rdata=.text
+
+if not exist bin md bin
+if not exist obj md obj
+if not exist boot md boot
+
+cl %CL_FLAGS% "kernel.cpp"
+cl %CL_FLAGS% "thirdparty\xvg\vga\vga.c"
+cl %CL_FLAGS% "thirdparty\xvg\sgl\sgl.c"
+
+link %LINK_FLAGS% "obj\kernel.obj" /OUT:"boot\kernel.exe"
+
+if errorlevel 1 exit /b 1
+
+"thirdparty\simpleboot\simpleboot.exe" -vv -c -k "kernel.exe" "boot" "bin\disk.img"
+qemu-system-x86_64 -drive file="bin\disk.img",format=raw -display sdl -vga std -serial stdio
+
+endlocal
--- run.bat
+++ run.bat
@@ -1,18 +1,23 @@
 @echo off
 
-set CLANG_FLAGS=-c --target=x86_64-elf -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -fno-strict-aliasing
+setlocal
+
+set CLANG_FLAGS=-c --target=x86_64-elf -ffreestanding -fno-exceptions -fno-rtti -fno-stack-protector -fno-strict-aliasing -fno-builtin
+set LD_FLAGS=-m elf_x86_64 --entry=_start --Ttext=0x100000 --image-base=0x100000 --omagic --nostdlib --static
 
 if not exist bin md bin
 if not exist obj md obj
 if not exist boot md boot
 
+clang++ %CLANG_FLAGS% "kernel.cpp" -o "obj\kernel.o"
 clang %CLANG_FLAGS% "thirdparty\xvg\vga\vga.c" -o "obj\vga.o"
 clang %CLANG_FLAGS% "thirdparty\xvg\sgl\sgl.c" -o "obj\sgl.o"
 
-clang++ %CLANG_FLAGS% "kernel.cpp" -o "obj\kernel.o"
+ld.lld %LD_FLAGS% "obj\kernel.o" "obj\vga.o" "obj\sgl.o" -o "boot\kernel.elf"
 
-ld.lld -m elf_x86_64 --entry=_start --Ttext=0x100000 --image-base=0x100000 --omagic --nostdlib --static "obj\kernel.o" "obj\vga.o" "obj\sgl.o" -o "boot\kernel.elf"
+if errorlevel 1 exit /b 1
 
-"thirdparty\simpleboot\simpleboot.exe" -vv -c "boot" "bin\disk.img"
-
+"thirdparty\simpleboot\simpleboot.exe" -vv -c -k "kernel.elf" "boot" "bin\disk.img"
 qemu-system-x86_64 -drive file="bin\disk.img",format=raw -display sdl -vga std -serial stdio
+
+endlocal
