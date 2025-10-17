60686e27d230908e7b3186f632bca6ee42e4361c
Julien QUENTIN <jqntn88@gmail.com>
2025-10-17 14:55:57 +0200

    rm memcpy asm

--- kernel.cpp
+++ kernel.cpp
@@ -37,7 +37,8 @@ extern "C"
 
   void memcpy(void* dst, const void* src, unsigned long n)
   {
-    asm volatile("repnz movsb" : : "D"(dst), "S"(src), "c"(n) :);
+    for (unsigned char *d = (unsigned char*)dst, *s = (unsigned char*)src; n--;)
+      *d++ = *s++;
   }
 
   void memset(void* dst, unsigned char c, unsigned long n)
